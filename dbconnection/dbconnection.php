c0594504d4e0de8a22d4e64b945ae237da5a8572
Clifford537 <cliffordmukosh@gmail.com>
2024-07-22 11:23:50 +0530

    updates

--- dbconnection/dbconnection.php
+++ dbconnection/dbconnection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "sql107.infinityfree.com";
 $username = "if0_36948684";
-$password = "rK71euqdiQms ";
+$password = "rK71euqdiQms";
 $dbname = "if0_36948684_ideas";
 
 try {
@@ -10,4 +10,19 @@ try {
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+
+/*
+//local server db configurations
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ideas";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+*/
 ?>
